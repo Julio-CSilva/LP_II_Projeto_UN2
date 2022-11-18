f3d0dc19fec6884bc7e200e93da0fc8707e0710c
Júlio Silva <julio.silva.706@ufrn.edu.br>
2022-11-18 10:06:36 -0300

    desenvolvendo parte superior do diagrama de classes

---
+++ SIGBombeiros/src/br/ufrn/imd/domain/Boletim/Boletim.java
@@ -0,0 +1,50 @@
+package br.ufrn.imd.domain.Boletim;
+
+import java.util.ArrayList;
+import br.ufrn.imd.domain.Alerta.Caso;
+
+public class Boletim {
+    
+    private Caso ultimoCaso;
+    private ArrayList<Noticia> noticias = new ArrayList<>();
+    
+    public Noticia criarNoticia(){
+
+    }
+
+    public void editarNoticia(Noticia noticia){
+
+    }
+
+    public Noticia excluirNoticia(Noticia noticia){
+
+    }
+
+    public void mudarUltimoCaso(Caso caso){
+
+    }
+
+    public void visualizarNoticias(){
+
+    }
+
+    public Boolean verificarAcesso(Pessoa pessoa){
+        
+    }
+
+    public Caso getUltimoCaso() {
+        return ultimoCaso;
+    }
+    public void setUltimoCaso(Caso ultimoCaso) {
+        this.ultimoCaso = ultimoCaso;
+    }
+    public ArrayList<Noticia> getNoticias() {
+        return noticias;
+    }
+    public void setNoticias(ArrayList<Noticia> noticias) {
+        this.noticias = noticias;
+    }
+
+    
+
+}
---
+++ SIGBombeiros/src/br/ufrn/imd/domain/Escala/Escala.java
@@ -0,0 +1,52 @@
+package br.ufrn.imd.domain.Escala;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import br.ufrn.imd.domain.Escala.Escala.Calendario;
+
+public class Escala {
+    private ArrayList<Calendario> escalasData = new ArrayList<>();
+    private ArrayList<Pessoa> escalasPessoa = new ArrayList<>();
+    private Date diaHoje;
+
+    public Calendario criarCalendario(){
+
+    }
+
+    public void editarCalendario(){
+
+    }
+
+    public void excluirCalendario(Calendario calendario){
+
+    } 
+
+    public void visualizarEscala(Pessoa pessoa){
+
+    }
+
+    public Boolean verificarAcesso(Pessoa pessoa){
+
+    }
+
+    public ArrayList<Calendario> getEscalasData() {
+        return escalasData;
+    }
+    public void setEscalasData(ArrayList<Calendario> escalasData) {
+        this.escalasData = escalasData;
+    }
+    public ArrayList<Pessoa> getEscalasPessoa() {
+        return escalasPessoa;
+    }
+    public void setEscalasPessoa(ArrayList<Pessoa> escalasPessoa) {
+        this.escalasPessoa = escalasPessoa;
+    }
+    public Date getDiaHoje() {
+        return diaHoje;
+    }
+    public void setDiaHoje(Date diaHoje) {
+        this.diaHoje = diaHoje;
+    }
+
+}
